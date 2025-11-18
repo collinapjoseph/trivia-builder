c67862df2ac7e5f50e4b33f8b25f069df928a5c9
collinapjoseph <collin.joseph.0258@gmail.com>
2025-11-17 20:18:52 -0500

    initial files

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const pool = pg.Pool(
+    {
+        user: 'postgres',
+        host: 'localhost',
+        database: 'trivia',
+        password: 'licence black supply',
+        port: 5432,
+    }
+);
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
